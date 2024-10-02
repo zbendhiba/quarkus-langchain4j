e0bbbc89d6d99634945fc40ae343261fcf2112a7
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2024-10-02 19:14:03 +0200

    demo status Devoxx Greece

---
+++ samples/sql-chatbot/src/main/java/io/quarkiverse/langchain4j/sample/chatbot/CsvIngestor.java
@@ -0,0 +1,20 @@
+package io.quarkiverse.langchain4j.sample.chatbot;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.BindyType;
+
+@ApplicationScoped
+public class CsvIngestor extends RouteBuilder {
+
+    public void configure() throws Exception {
+
+        // ingesting a CSV files listing the top 100 movies from IMDB
+        from("file:src/main/resources/data?fileName=movies.csv&noop=true")
+                .log("**** ingesting a CSV files listing the top 100 movies from IMDB")
+                .unmarshal().bindy(BindyType.Csv, Movie.class)
+                .split(body())
+                .streaming()
+                .to("jpa:" + Movie.class.getName());
+    }
+}
